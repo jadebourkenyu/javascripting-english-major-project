186a64fe560bc8405621e9d46a18ef789b2b63d5
Jade Bourke <jadebourke@Jades-MacBook-Pro-2.local>
2017-10-11 10:40:39 -0400

    I could get the var(1,2,3) to retrun 2,4,6 to me idk why. I tried putting it in index html but that didnt work either.

--- scripts.js
+++ scripts.js
@@ -2,3 +2,8 @@ var numbers = [1,2,3]
 var doubles = nmbers.map(function(x){
   return x*2;
 })
+
+if (turtle.name.includes("o")){
+  return turtle.name;
+}
+// this is not a good idea because it is an easy way out and won't work for everyt case.If you hade a list of things this wouldn't work because your list migh contain every letter. 
