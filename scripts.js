51a6c8fc0c41db3cbb953f2e9450d5d9cbd65387
Jade Bourke <jadebourke@Jades-MacBook-Pro-2.local>
2017-11-20 11:29:19 -0500

    hw. I pasted my table in here but the coding didnt come up like in class

--- scripts.js
+++ scripts.js
@@ -1,9 +1,104 @@
-var numbers = [1,2,3]
-var doubles = nmbers.map(function(x){
-  return x*2;
-})
 
-if (turtle.name.includes("o")){
-  return turtle.name;
-}
-// this is not a good idea because it is an easy way out and won't work for everyt case.If you hade a list of things this wouldn't work because your list migh contain every letter. 
+Location
+Lat.
+Long.
+Description
+Morningside Heights
+/Morningside park
+
+
+
+
+Starts his walk here
+..is here again in chapt 18 he noticed shadows it was two young men whi didnt smile back
+Central Park South
+
+
+
+
+Walks by often
+Columbus Circle
+
+
+
+
+Saw masses of people
+Lincoln Center
+
+
+
+
+Walks towards
+Amsterdam Ave
+
+
+
+
+Walking by and sees alot of people
+Riverside Park- 116th st
+
+
+
+
+He walks on a warm night
+Sheep Meadow
+
+
+
+
+Goes two days in a row.
+Masses of leaves dying off in bright colors
+Hears birds
+Wet from rain earlier
+Lexington Ave
+
+
+
+
+Goes North here and reaches the lecture hall he saw poetry
+World Trade Center
+
+
+
+
+Has memories of being tourist asking abt 9/11. The other side of the highway was tranquil and there were neon lights
+
+
+Kennedy Airport
+
+
+
+
+Goes to Brussels
+
+
+Corner of Water st and Wall st
+
+
+
+
+His mind was empty subject to nervous conditions
+Battery Park
+
+
+
+
+Clear day. He could see right across to Brooklyn, staten island, and statue of liberty.
+Washington Heights
+
+
+
+
+John Musson’s apartment. North of the hospital
+181st and Cabrini
+
+
+
+
+Stops here for coffee after Johns
+Carnegie Hall
+
+
+
+
+Where he sees the ninth symphony
